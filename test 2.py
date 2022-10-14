d0584a60beec382343c23005702c84dc19d4a5aa
Oleg Karbaev <koa3103@gmail.com>
2022-10-14 22:41:55 +0300

    14.10.2022

--- test 2.py
+++ test 2.py
@@ -1,10 +1,8 @@
-for key, value in directories.items():
-    if shelf_number == key:
-        value.append(number_document)
-        result = (" Успешно добавлен: \n документ: {} \n №: {} \n вдалелец: {} \n № полки: {}\n".format(
-            type_document, number_document, owner_name_document, shelf_number))
-
-
-for key, value in directories.items():
-    if shelf_number == key:
-        value.append(number_document)
+k = [800, 100, 2, 4, 6, 3, 9]
+for i in k:
+    if i % 2 == 0:
+        # print(i)
+        k.remove(i)
+    else:
+        # k.extend(i)
+print(k)
